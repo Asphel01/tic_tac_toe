49d47424d7366aa7ae2b5d2c6b75a4f783266bf4
Asphel01 <raymond19992010@hotmail.com>
2023-10-01 19:55:35 -0400

    completion of a basic tic tac toe brower game

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,14 +1,22 @@
+const form = document.querySelectorAll(".playerInput");
+const p1Input = document.querySelector("#player1");
+const p2Input = document.querySelector("#player2");
+
 const gameBoard = (() => {
-    let board = [[0 ,0, 0][0, 0, 0][0, 0, 0]];
+    let board = [[0, 0, 0], [0 , 0, 0], [0, 0, 0]];
 
     const setBoard = (character, row, col) => {
-        if(!board[row][col] === 0) {
+        if(board[row][col] === 0) {
             board[row][col] = character;
         }
     }
 
     const getCharacter = (row, col) => {
         return board[row][col];
+    }
+
+    const resetBoard = () => {
+        board = [[0, 0, 0], [0 , 0, 0], [0, 0, 0]];
     }
 
     const isFilled = () => {
@@ -24,33 +32,38 @@ const gameBoard = (() => {
 
     const checkStatus = () => {
         for(let i = 0; i < board.length; i++) {
-            if(board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            if(board[i][0] !== 0 && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
                 return board[i][0];
             }
 
-            if(board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            if(board[0][i] !== 0 && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
                 return board[0][i];
             }               
         }
 
-        if(board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        if(board[0][0] !== 0 && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
             return board[0][0];
         }
 
-        if(board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        if(board[0][2] !== 0 && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
             return board[0][2];
         }
 
         if(isFilled()) {
-            return "No Winner";
+            return "No winner";
         }
     }
 
-    return {setBoard, getCharacter, checkStatus};
+    return {setBoard, resetBoard, getCharacter, checkStatus};
 })();
 
 const player = character => {
     let myTurn;
+    const myChar = character;
+
+    const getCharacter = () => {
+        return myChar;
+    }
 
     const setTurn = () => {
         myTurn = true;
@@ -64,35 +77,66 @@ const player = character => {
         return myTurn;
     }
 
-    return {setTurn, passTurn, isMyTurn};
+    return {getCharacter, setTurn, passTurn, isMyTurn};
 }
 
+const player1 = player("X");
+const player2 = player("O");
+
 const gameLogic = ((gameBoard, player1, player2) => {
-    const turnCounter = 0;
+    const container = document.querySelector(".gridContainer");
     const gridSquares = document.querySelectorAll(".square");
+    const winDisplay = document.querySelector(".winnerDisplay");
+    const resetBtn = document.querySelector(".reset");
+    const startBtn = document.querySelector(".startBtn");
+
+    startBtn.addEventListener('click', event => {
+        form.forEach(box => {box.style.display = "none"});
+        startBtn.style.display = "none";
+        container.style.visibility = "visible";
+        resetBtn.style.visibility = "visible";
+        player1.setTurn();
+        player2.passTurn();
+    });
+
     gridSquares.forEach(square => {
-        square.addEventListener('click', () => {
+        square.addEventListener('click', event => {
             const row = square.getAttribute("id").charAt(0);
             const col = square.getAttribute("id").charAt(1);
-            const currentChar = gameBoard.getCharacter(row, col);
+            const currentChar = gameBoard.getCharacter(parseInt(row), parseInt(col));
+
             if(currentChar === 0 && player1.isMyTurn()) {
                 gameBoard.setBoard(player1.getCharacter(), row, col);
-                square.textContent(player1.getCharacter());
-                turnCounter++;
-            }
-            if(currentChar === 0 && player2.isMyTurn()) {
+                square.textContent = player1.getCharacter();
+                player1.passTurn();
+                player2.setTurn();
+            }else if(currentChar === 0 && player2.isMyTurn()) {
                 gameBoard.setBoard(player2.getCharacter(), row, col);
-                square.textContent(player2.getCharacter());
-                turnCounter++;
+                square.textContent = player2.getCharacter();
+                player2.passTurn();
+                player1.setTurn();                
             }    
+
+            let status = gameBoard.checkStatus();
+            if(status !== undefined) {
+                if(status === "No winner") {
+                    winDisplay.textContent = status;
+                }else if(status === "X"){
+                    winDisplay.textContent =  p1Input.value + " is the winner";
+                }else {
+                    winDisplay.textContent =  p2Input.value + " is the winner";
+                }
+            }
         });    
     });
 
-
-    //add checker
-})();
-
-const player1 = player("X");
-player1.setTurn();
-const player2 = player("O");
-player2.passTurn();
+    resetBtn.addEventListener('click', event => {
+        gridSquares.forEach(square => {
+            square.textContent = "";
+        });
+        gameBoard.resetBoard();
+        winDisplay.textContent = "";
+        player1.setTurn();
+        player2.passTurn();
+    });
+})(gameBoard, player1, player2);
